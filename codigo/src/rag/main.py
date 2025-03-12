fdcde268dfe1ec287e251fbc7b9cdea31b4db9ee
Helaine Barreiros <helaine.lins@upe.br>
2025-03-12 15:22:49 -0300

    ajustes na tokenizacao em paragrafos

--- codigo/src/rag/main.py
+++ codigo/src/rag/main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
 from document_loader import load_documents
 from embedding_generator import process_and_store_embeddings
 
